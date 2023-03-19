19cbb5171eed005e19ac5eeb109e492f30f46b27
Tim Norman <tim@timnorman.ca>
2023-03-19 12:07:36 -0400

    Created gameplay loop method. Tested to work.

--- src/GuessingGame.java
+++ src/GuessingGame.java
@@ -4,17 +4,18 @@ import java.util.Scanner;
 public class GuessingGame {
     Scanner keyboard = new Scanner(System.in);
     private int guessCount = 0;
-    private int upperBound = 100;   // Maximum (inclusive) bound for number to be.
+    private final int UPPER_BOUND = 100;   // Maximum (inclusive) bound for number to be.
     int numberToGuess;
 
 
     private void createRandomNumber(){
         Random rnd = new Random();
-        numberToGuess = rnd.nextInt(upperBound);
+        numberToGuess = rnd.nextInt(UPPER_BOUND);
     }
 
     private void greeting(){
-        System.out.printf("Choose a number between 1 and %d.", upperBound);
+        createRandomNumber();
+        System.out.printf("Choose a number between 1 and %d.%n", UPPER_BOUND);
     }
     private int promptForGuess(){
         System.out.print("What is your guess? ");
@@ -24,21 +25,30 @@ public class GuessingGame {
         return userGuess;
     }
 
-    private boolean checkGuess(int userGuess){
+    private boolean continueGuessing(int userGuess){
         if(userGuess == numberToGuess){
             System.out.println("Congratulations! You guessed the right answer which was " + numberToGuess);
-            return true;
+            System.out.println("It took " + guessCount + " tries.");
+            return false;
         }
         else if(userGuess < numberToGuess){
             System.out.println("Try again. Guess higher.");
-
+            return true;
         }
         else if(userGuess > numberToGuess){
             System.out.println("Try again. Guess lower.");
-
+            return true;
         }
-        return false;
+        return true;
     }
 
-    
+    private void gameLoop(){
+        while(continueGuessing(promptForGuess())){
+        }
+    }
+
+    public void playGame(){
+        greeting();
+        gameLoop();
+    }
 }
